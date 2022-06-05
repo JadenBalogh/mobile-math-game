8e9cfc17fb3211c7136f4db23ff6eb5e2237698a
Jaden Balogh <jadenbalogh@gmail.com>
2022-06-05 17:01:06 -0400

    Add exploding player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Actor
 {
     private static Player instance;
     public static Player Instance { get => instance; }
@@ -14,11 +14,14 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveTime = 1f;
 
+    [SerializeField] private int startStrength = 1;
+
     public Transform Target { get; set; }
+    public int CurrentStrength { get; set; }
 
     private Vector2 currVelocity;
 
-    private void Awake()
+    protected override void Awake()
     {
         if (instance != null)
         {
@@ -28,6 +31,9 @@ public class Player : MonoBehaviour
         instance = this;
 
         Target = startPos;
+        CurrentStrength = startStrength;
+
+        base.Awake();
     }
 
     private void Update()
@@ -38,5 +44,17 @@ public class Player : MonoBehaviour
     public bool IsMoving()
     {
         return (transform.position - Target.position).sqrMagnitude > 0.01f;
+    }
+
+    public void Fight(Enemy enemy)
+    {
+        if (CurrentStrength >= enemy.Strength)
+        {
+            enemy.Explode();
+        }
+        else
+        {
+            Explode();
+        }
     }
 }
